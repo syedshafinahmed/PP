feb3e13db78ee80ea88e1f4e7029b76ac13ab48e
syedshafinahmed <shafinahmed.cse@gmail.com>
2025-12-04 01:08:49 +0600

    Updated Projects

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -59,7 +59,7 @@ const projectsData = [
     title: "GreenNest",
     description:
       "GreenNest – An elegant single-page web application built for plant lovers who want to nurture and decorate their homes with healthy indoor plants. The platform allows users to explore plant care guides, buy plants, and book expert consultations — ensuring a greener and healthier living space during any season.",
-    image: "https://i.ibb.co.com/mr8TZ2GV/Screenshot-2025-10-30-003043.png",
+    image: "https://i.ibb.co.com/8GvsvLS/greennest-ssa-web-app-1.png",
     technologies: ["React", "Tailwind CSS", "JavaScript", "Firebase", "API Integration"],
     liveDemo: "https://greennest-syedshafinahmed.pages.dev/",
     sourceCode: "https://github.com/syedshafinahmed/GreenNest",
