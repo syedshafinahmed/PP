c4270c4aaf847bdd7d886ad46a63dbf7010cfef0
syedshafinahmed <shafinahmed.cse@gmail.com>
2025-12-04 20:09:59 +0600

    Updated Hero section

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -3,6 +3,7 @@ import me from '../assets/me.jpg';
 import { FiGithub } from 'react-icons/fi';
 import { SlSocialLinkedin } from 'react-icons/sl';
 import { TypeAnimation } from 'react-type-animation';
+import { FaGithubSquare, FaLinkedin } from 'react-icons/fa';
 
 const Hero = () => {
   return (
@@ -12,27 +13,51 @@ const Hero = () => {
           <div className="hero-content flex-col lg:flex-row-reverse">
             <img src={me} data-aos="fade-left" className="max-w-sm mb-7 md:mb-0 rounded-full shadow-2xl" />
             <div data-aos="fade-right">
-              <h1 className="text-3xl md:text-5xl text-center md:text-left font-bold"><span className='text-gray-400'>Hi, This is </span> <TypeAnimation sequence={['Syed Shafin Ahmed', 1500, '', 500,]} speed={70} repeat={Infinity} cursor={false} /></h1>
+              <div className="text-center md:text-left font-bold">
+                <h1 className="text-3xl md:text-5xl">
+                  <span className="text-gray-400">Syed Shafin Ahmed</span>
+                </h1>
+
+                <div className="h-6 text-2xl mt-4 flex justify-center md:justify-start items-center">
+                  <TypeAnimation
+                    className="text-2xl"
+                    sequence={[
+                      'Frontend Developer',
+                      1500,
+                      'React Developer',
+                      1500,
+                      'MERN Stack Developer',
+                      1500,
+                    ]}
+                    speed={70}
+                    repeat={Infinity}
+                    cursor={false}
+                  />
+                </div>
+              </div>
+
+
               <p className="text-sm md:text-lg py-6 px-10 md:px-0 text-center md:text-left text-gray-400">
                 An aspiring Full-Stack Developer with a strong foundation in frontend development and a growing focus on backend architecture and APIs.
               </p>
 
               <div className="flex justify-center items-center md:justify-start gap-5 mb-10 md:mb-0">
+                <a href='/Syed Shafin Ahmed.pdf' download className='btn text-black border-none bg-[#F4A24C] text-base-200 btn-sm'>Download Resume</a>
                 <a
                   href="https://github.com/syedshafinahmed"
                   target="_blank"
                   rel="noopener noreferrer"
-                  className="text-3xl text-gray-400 hover:text-orange-300 transition-all duration-300 hover:drop-shadow-[0_0_30px_rgba(255,215,0,1)]"
+                  className="text-3xl text-[#F4A24C] hover:text-[#F4A24C] transition-all duration-300 hover:drop-shadow-[0_0_30px_rgba(255,215,0,1)]"
                 >
-                  <FiGithub size={25} />
+                  <FaGithubSquare size={30} />
                 </a>
                 <a
                   href="https://www.linkedin.com/in/syed-shafin-ahmed/"
                   target="_blank"
                   rel="noopener noreferrer"
-                  className="text-3xl text-gray-400 hover:text-orange-300 transition-all duration-300 hover:drop-shadow-[0_0_30px_rgba(255,215,0,1)]"
+                  className="text-3xl text-[#F4A24C] hover:text-[#F4A24C] transition-all duration-300 hover:drop-shadow-[0_0_30px_rgba(255,215,0,1)]"
                 >
-                  <SlSocialLinkedin size={25} />
+                  <FaLinkedin size={30} />
                 </a>
               </div>
 
